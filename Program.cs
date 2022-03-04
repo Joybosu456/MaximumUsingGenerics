538fa6f494e726634dd163eb695ed5474c0cd94d
JOY <joybosu@gmail.com>
2022-03-04 21:41:32 +0530

    Add program file more than three value

--- Program.cs
+++ Program.cs
@@ -10,12 +10,12 @@ namespace CheckMax
             //Console.WriteLine("Hello welcome to Generics Problem");
             //int output = Maximum.MaxIntNum(2,5,7);
             //Console.WriteLine(output);
-            
-            //double output = FloatValue.MaxIntNum(1.6,3.2,9.8);
-           // Console.WriteLine(output);
 
-            string output = StringValue.MaxString("AC","MO","PZ");
+            double output = FloatValue.MaxIntNum(1.6, 3.2, 9.8,71.2);
             Console.WriteLine(output);
+
+            //string output = StringValue.MaxString("AC","MO","PZ");
+            //Console.WriteLine(output);
         }
 
 
