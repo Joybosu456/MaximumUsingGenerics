cb94109f496a0c3f829df8e1ef0a4ecddfc3ea73
JOY <joybosu@gmail.com>
2022-03-04 21:42:45 +0530

    compare with for floats value

--- FloatValue.cs
+++ FloatValue.cs
@@ -8,25 +8,30 @@ namespace CheckMax
 {
     public class FloatValue
     {
-        public static double MaxIntNum(double firstValue, double secoundValue, double thirdValue)
+        public static double MaxIntNum(double firstValue, double secoundValue, double thirdValue, double forthValue)
         {
-            if (firstValue.CompareTo(secoundValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            if (firstValue.CompareTo(secoundValue) > 0 && firstValue.CompareTo(thirdValue) > 0 && firstValue.CompareTo(forthValue) > 0)
             //firstValue.CompareTo(secoundValue) >= 0 && firstValue.CompareTo(thirdValue) > 0 ||
             //firstValue.CompareTo(secoundValue) > 0 && firstValue.CompareTo(thirdValue) >= 0)
             {
                 return firstValue;
             }
-            if (secoundValue.CompareTo(firstValue) > 0 && secoundValue.CompareTo(thirdValue) > 0)
+            if (secoundValue.CompareTo(firstValue) > 0 && secoundValue.CompareTo(thirdValue) > 0 && secoundValue.CompareTo(forthValue) > 0)
             //secoundValue.CompareTo(firstValue) >= 0 && secoundValue.CompareTo(thirdValue) > 0 ||
             //secoundValue.CompareTo(firstValue) > 0 && secoundValue.CompareTo(thirdValue) >= 0)
             {
                 return secoundValue;
             }
-            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(firstValue) > 0)
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secoundValue) > 0 && thirdValue.CompareTo(forthValue) > 0)
             //thirdValue.CompareTo(firstValue) >= 0 && thirdValue.CompareTo(firstValue) > 0 ||
             //thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(firstValue) >= 0)
             {
                 return thirdValue;
+            }
+            
+            if (forthValue.CompareTo(firstValue) > 0 && forthValue.CompareTo(secoundValue) > 0 && forthValue.CompareTo(thirdValue) > 0)
+            {
+                return forthValue;
             }
             return firstValue;
 
