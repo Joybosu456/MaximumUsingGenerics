40fbe742019db768331722b871ec2ecaf9214f5f
JOY <joybosu@gmail.com>
2022-03-04 19:40:01 +0530

    Add generics welcome Program

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CheckMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello welcome to Generics Problem");
+            //int output = Maximum.MaxIntNum(25, 14, 60);
+            //Console.WriteLine(output);                                     
+        }
+
+
+    }
+
+    
+}
